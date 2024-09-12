2c9f2f4b0d7f40e1c05e8d5750b07105a95f1a1b
Samuel Sant Anna <samuelsantaanna@gmail.com>
2024-09-12 20:31:06 -0300

    2 3617 BE finished

--- src/http/server.ts
+++ src/http/server.ts
@@ -4,12 +4,19 @@ import { createGoalRoute } from "./routes/create-goal";
 import { createCompletionGoalRoute } from "./routes/create-completion";
 import { getWeeklyPendingGoalsRoute } from "./routes/get-pending-goals";
 import { getWeekSummaryRoute } from "./routes/get-week-summary";
+import fastifyCors from "@fastify/cors";
+import { register } from "module";
 
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
+app.register(fastifyCors, {
+    origin: '*',
+})
+
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
 app.register(createGoalRoute)
 app.register(createCompletionGoalRoute)
 app.register(getWeeklyPendingGoalsRoute)
