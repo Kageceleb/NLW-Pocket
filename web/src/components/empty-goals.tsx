1c62302a107a5e4b47d9cd81ee228053fc3b85f2
Samuel Sant Anna <samuelsantaanna@gmail.com>
2024-09-13 19:51:55 -0300

    start of 3rd lesson

--- web/src/components/empty-goals.tsx
+++ web/src/components/empty-goals.tsx
@@ -4,20 +4,20 @@ import { DialogTrigger } from "./ui/dialog";
 import logo from "../assets/logo-in-orbit.svg"
 import letsStart from "../assets/lets-start-illustration.svg"
 
-export function EmptyGoals(){
-    return(
-        <div className="h-screen flex flex-col items-center justify-center gap-8">
-        <img src={logo} alt="in orbit logo" />
-        <img src={letsStart} alt="let start" />
-        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-          Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
-        </p>
-        <DialogTrigger asChild>
-          <Button>
-            <Plus className='size-4' />
-            Cadastrar meta
-          </Button>
-        </DialogTrigger>
-      </div>
-    )
+export function EmptyGoals() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in orbit logo" />
+      <img src={letsStart} alt="let start" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+      <DialogTrigger asChild>
+        <Button>
+          <Plus className='size-4' />
+          Cadastrar meta
+        </Button>
+      </DialogTrigger>
+    </div>
+  )
 }
